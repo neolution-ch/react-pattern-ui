704cc411fbc7009ab59ff5900789f8795ae20d03
Timothy Trowbridge <timothy.trowbridge@neolution.ch>
2022-04-07 11:16:19 -0400

    react jsx

--- src/Paging/Paging.tsx
+++ src/Paging/Paging.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { Button, ButtonGroup, Col, DropdownItem, DropdownMenu, DropdownToggle, Row, UncontrolledButtonDropdown } from "reactstrap";
 
 interface PagingProps {
