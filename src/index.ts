fc803e767f5cedf5e48248131e643bc3894624bd
Oscar Gerosa <oscar.gerosa@neolution.ch>
2022-11-17 09:34:49 +0100

    Login layout component

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export { DeleteAction, DeleteActionProps } from "./DeleteAction/DeleteAction";
 export { DateHandler } from "./Utils/DateHandler";
+export { LoginLayout } from "./Layout/LoginLayout";
 export { Paging } from "./Paging/Paging";
