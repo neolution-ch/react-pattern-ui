5c758709dca49fda29af6973bc07447ea9998967
Oscar Gerosa <oscar.gerosa@neolution.ch>
2022-11-29 15:11:48 +0100

    Layout changes

--- src/Layout/SideBarLayout/SideBarLayout.tsx
+++ src/Layout/SideBarLayout/SideBarLayout.tsx
@@ -23,13 +23,13 @@ export const SideBarLayout = (props: SideBarLayoutProps) => {
   return (
     <>
       <nav id="nav-top" className="navbar navbar-expand navbar-dark">
+        {/* Navbar Brand*/}
+        <div className="navbar-brand">{brand ?? contextBrand}</div>
+        
         {/* Sidebar Toggle*/}
         <button id="sidebar-toggle" className="btn btn-link btn-sm order-0 me-lg-0" onClick={() => toggleSidebar()}>
           <FontAwesomeIcon icon={faBars} size="2x" />
         </button>
-
-        {/* Navbar Brand*/}
-        <div className="navbar-brand">{brand ?? contextBrand}</div>
 
         {/* <NavbarUser /> */}
       </nav>
