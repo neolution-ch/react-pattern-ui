21a4f00a71d3fdd0cdb7b582f8fbe021469a9774
Gianmarco Manni <gianmarco.manni@neolution.ch>
2023-03-24 11:37:44 +0100

    undo push

--- src/Layout/PanelSideBarLayout/PanelSideBar/Context/PanelSideBarContext.tsx
+++ src/Layout/PanelSideBarLayout/PanelSideBar/Context/PanelSideBarContext.tsx
@@ -34,7 +34,6 @@ export interface PanelSideBarMenuProviderProps<TPanelItem, TMenuItem>
 export const PanelSideBarProvider = <TPanelItem, TMenuItem>(props: PanelSideBarMenuProviderProps<TPanelItem, TMenuItem>) => {
   const { children, globalItems, LinkRenderer } = props;
 
-    console.log("I can push");
   const [toggledMenuItemIds, setToggledMenuItemIds] = useState<string[]>([]);
 
   const toggleMenuItem: MenuItemToggleFn<TMenuItem> = (menuItem) => {
