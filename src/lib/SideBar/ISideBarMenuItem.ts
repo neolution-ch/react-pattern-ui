15456df38b1c612357124df55368e929f7415387
FW <florian.wermelinger@neolution.ch>
2023-08-25 17:39:03 +0200

    fixed menu-open class and added styling options (#31)
    * fix: class menu-open will only be added to items with children
    * added option to provide additional classes to menu items
    * added styles to highlight hovering and open menus,

--- src/lib/SideBar/ISideBarMenuItem.ts
+++ src/lib/SideBar/ISideBarMenuItem.ts
@@ -5,12 +5,24 @@ export interface ISideBarMenuItem {
   title: string;
   icon?: IconProp;
   children?: ISideBarMenuItem[];
+
   /**
    * Whether the item is expanded.
    */
   expanded?: boolean;
+
   /**
    * Whether the item should be displayed.
    */
   display?: boolean;
+
+  /**
+   * classes that can be added by the user
+   */
+  className?: string;
+
+  /**
+   * adds the active class to the element
+   */
+  isActive?: boolean;
 }
