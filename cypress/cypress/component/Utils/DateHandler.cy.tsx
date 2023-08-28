6860c4bc7ca15b6e20b9ec6b0742c968a383dbb5
SeverinoAmmirati <severino.ammirati@neolution.ch>
2023-08-28 14:23:51 +0200

    Test HH format in date handler

--- cypress/cypress/component/Utils/DateHandler.cy.tsx
+++ cypress/cypress/component/Utils/DateHandler.cy.tsx
@@ -9,6 +9,7 @@ describe("DateHandler.cy.tsx", () => {
   it("get date string formated", async () => {
     DateHandler.setMonthTranslationMethod((month) => (month === 11 ? "November" : month.toString()));
     expect(DateHandler.getDateFormatted(new Date(2022, 10, 15, 12, 33), "dd MMMM yyyy hh:mm")).to.eq("15 November 2022 12:33");
+    expect(DateHandler.getDateFormatted(new Date(2022, 10, 15, 12, 22), "dd MMMM yyyy HH:mm")).to.eq("15 November 2022 12:22");
   });
 
   it("get date string default", async () => {
