a53cee965bc30e470e50b279e0a9cb55245e4234
Nicolò Traini <nicolo.traini@neolution.ch>
2023-09-19 12:52:42 +0200

    update test

--- cypress/cypress/component/DeleteAction/DeleteAction.cy.tsx
+++ cypress/cypress/component/DeleteAction/DeleteAction.cy.tsx
@@ -4,6 +4,7 @@ import { faker } from "@faker-js/faker";
 
 describe("DeleteAction.cy.tsx", () => {
   it("delete action works", () => {
+    const buttonText = faker.random.word();
     const deleteButtonText = faker.random.word();
     const cancelButtonText = faker.random.word();
     const text = faker.random.words(10);
@@ -12,23 +13,24 @@ describe("DeleteAction.cy.tsx", () => {
     cy.mount(
       <DeleteAction
         onDelete={cy.spy().as("onDelete")}
+        buttonText={buttonText}
         deleteButtonText={deleteButtonText}
         cancelButtonText={cancelButtonText}
         text={text}
         title={title}
-        iconOnly
       />,
     );
 
-    // Check if correct default icon is drawn, open dialog, check title and press close icon
-    cy.get("[data-cy-root] > svg").should("have.class", "fa-trash").click();
+    // Check if correct default icon and text are drawn, open dialog, check title and press close icon
+    cy.get("[data-cy-root]").parents("body").get(".btn-danger").should("have.text", buttonText);
+    cy.get("[data-cy-root] svg").should("have.class", "fa-trash").click();
     cy.get("[data-cy-root]").parents("body").get(".modal-dialog .modal-header h5").should("have.text", title);
     cy.get("[data-cy-root]").parents("body").get(".modal-dialog .modal-header button.btn-close").click();
     cy.get("@onDelete").should("not.be.calledOn");
     cy.get("[data-cy-root]").parents("body").get(".modal-dialog").should("not.exist");
 
     // Open dialog, check text, cancel button and press cancel button
-    cy.get("[data-cy-root] > svg").should("have.class", "fa-trash").click();
+    cy.get("[data-cy-root] svg").should("have.class", "fa-trash").click();
     cy.get("[data-cy-root]").parents("body").get(".modal-dialog .modal-body").should("have.text", text);
     cy.get("[data-cy-root]")
       .parents("body")
@@ -39,7 +41,7 @@ describe("DeleteAction.cy.tsx", () => {
     cy.get("[data-cy-root]").parents("body").get(".modal-dialog").should("not.exist");
 
     // Open dialog, check delete button and press delete button
-    cy.get("[data-cy-root] > svg").should("have.class", "fa-trash").click();
+    cy.get("[data-cy-root] svg").should("have.class", "fa-trash").click();
     cy.get("[data-cy-root]").parents("body").get(".modal-dialog .modal-footer .btn-danger").should("have.text", deleteButtonText).click();
     cy.get("@onDelete").should("be.calledOnce");
   });
