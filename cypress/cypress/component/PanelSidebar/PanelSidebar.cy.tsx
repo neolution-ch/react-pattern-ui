85e40451b9741488e335d333bf3ff33b9e72806c
Gianmarco Manni <gianmarco.manni@neolution.ch>
2024-05-13 11:44:38 +0200

    additional tests

--- cypress/cypress/component/PanelSidebar/PanelSidebar.cy.tsx
+++ cypress/cypress/component/PanelSidebar/PanelSidebar.cy.tsx
@@ -1,34 +1,42 @@
 import React from "react";
 import { PanelSideBarProvider, PanelSideBarLayout, PanelItem, PanelLinkRendererProps } from "react-pattern-ui";
 import { faBars, faCogs } from "@fortawesome/free-solid-svg-icons";
-import { IconProp } from "@fortawesome/fontawesome-svg-core";
 
 type AppRoutes = "home" | "settings" | "dropdownTest" | "dropdown-test1" | "dropdown-test2";
-
 type TSideBarMenuItem = PanelItem<AppRoutes>;
 
-const PanelSidebar = (items: TSideBarMenuItem[]) => (
-  <PanelSideBarProvider
-    menuItems={items}
-    LinkRenderer={(elem: PanelLinkRendererProps<AppRoutes, Record<string, unknown>>) => (
-      <div
-        id={elem.item.id}
-        onClick={() => {
-          const pageContent = document.getElementById("pageContent");
-          if (pageContent) {
-            pageContent.innerText = elem.item.id;
-          }
-        }}
-      >
-        <>{elem.item.title}</>
-      </div>
-    )}
-  >
-    <PanelSideBarLayout>
-      <div id="pageContent">Cypress</div>
-    </PanelSideBarLayout>
-  </PanelSideBarProvider>
-);
+// Configuration object for avoiding duplicated code
+interface PanelSideBarConfiguration {
+  renderFirstItemsLevelAsTiles?: boolean;
+  useToggleButton?: boolean;
+}
+
+const getPanelSidebarInternal = (items: TSideBarMenuItem[], config?: PanelSideBarConfiguration) => {
+  const { renderFirstItemsLevelAsTiles = true, useToggleButton = false } = config ?? {};
+  return (
+    <PanelSideBarProvider
+      menuItems={items}
+      renderFirstItemsLevelAsTiles={renderFirstItemsLevelAsTiles}
+      LinkRenderer={(elem: PanelLinkRendererProps<AppRoutes, Record<string, unknown>>) => (
+        <div
+          id={elem.item.id}
+          onClick={() => {
+            const pageContent = document.getElementById("pageContent");
+            if (pageContent) {
+              pageContent.innerText = elem.item.id;
+            }
+          }}
+        >
+          <>{elem.item.title}</>
+        </div>
+      )}
+    >
+      <PanelSideBarLayout useToggleButton={useToggleButton}>
+        <div id="pageContent">Cypress</div>
+      </PanelSideBarLayout>
+    </PanelSideBarProvider>
+  );
+};
 
 const getSidebarItems = (active?: boolean, disabled?: boolean): TSideBarMenuItem[] => [
   {
@@ -73,10 +81,20 @@ const getSidebarItems = (active?: boolean, disabled?: boolean): TSideBarMenuItem
   },
 ];
 
+const PanelSideBarWithTiles = (props: { active?: boolean; disabled?: boolean }) => {
+  const { active, disabled } = props;
+  return getPanelSidebarInternal(getSidebarItems(active, disabled));
+};
+
+const PanelSideBarNoTiles = (props: { active?: boolean; disabled?: boolean }) => {
+  const { active, disabled } = props;
+  return getPanelSidebarInternal(getSidebarItems(active, disabled), { renderFirstItemsLevelAsTiles: false, useToggleButton: true });
+};
+
 describe("PanelSidebar.cy.tsx", () => {
   it("icon and titles rendered correctly", () => {
     const sidebarItems = getSidebarItems();
-    cy.mount(PanelSidebar(sidebarItems));
+    cy.mount(<PanelSideBarWithTiles />);
 
     // Check if icon are rendered
     cy.get('[data-icon="bars"]').should("be.visible");
@@ -89,8 +107,7 @@ describe("PanelSidebar.cy.tsx", () => {
   });
 
   it("flat menu entry", () => {
-    const sidebarItems = getSidebarItems();
-    cy.mount(PanelSidebar(sidebarItems));
+    cy.mount(<PanelSideBarWithTiles />);
 
     // Check page content changes
     cy.get("#home").click();
@@ -101,8 +118,7 @@ describe("PanelSidebar.cy.tsx", () => {
   });
 
   it("nested menu entries", () => {
-    const sidebarItems = getSidebarItems();
-    cy.mount(PanelSidebar(sidebarItems));
+    cy.mount(<PanelSideBarWithTiles />);
 
     // Check page content changes
     cy.get("button[title=Settings]").click();
@@ -115,8 +131,7 @@ describe("PanelSidebar.cy.tsx", () => {
   });
 
   it("disabled entries", () => {
-    const sidebarItems = getSidebarItems(false, true);
-    cy.mount(PanelSidebar(sidebarItems));
+    cy.mount(<PanelSideBarWithTiles disabled />);
 
     // Check are disabled and page content not doesn't change
     cy.get("button[title=Home]").should("have.attr", "disabled");
@@ -129,7 +144,7 @@ describe("PanelSidebar.cy.tsx", () => {
 
   it("toggle sidebar", () => {
     const sidebarItems = getSidebarItems();
-    cy.mount(PanelSidebar(sidebarItems));
+    cy.mount(<PanelSideBarWithTiles />);
 
     // Check toggle sidebar
     cy.get('[data-icon="angle-left"]').should("be.visible");
@@ -140,10 +155,21 @@ describe("PanelSidebar.cy.tsx", () => {
   });
 
   it("selected flat entry", () => {
-    const sidebarItems = getSidebarItems(true);
-    cy.mount(PanelSidebar(sidebarItems));
+    cy.mount(<PanelSideBarWithTiles active />);
 
     // Check active entries
     cy.get("#home").parent("div").parent("li").should("have.class", "active");
+  });
+
+  it("render correctly menu without tiles", () => {
+    cy.mount(<PanelSideBarNoTiles />);
+    cy.get("#main-section").should("have.class", "section-no-tiles");
+    cy.get(".side.nav__tiles").should("not.exist");
+  });
+
+  it("render correctly toggle button", () => {
+    cy.mount(<PanelSideBarNoTiles />);
+    cy.get("#sidebar-toggle").click();
+    cy.get("#side-nav").should("have.css", "width", "0px");
   });
 });
