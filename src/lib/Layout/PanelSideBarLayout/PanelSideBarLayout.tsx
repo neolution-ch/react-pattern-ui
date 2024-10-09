af09f0974e57b47abe79fa58d67e71a4b1d589fa
Timothy Trowbridge <timothy.trowbridge@neolution.ch>
2024-10-09 09:53:12 -0300

    hello from timothy

--- src/lib/Layout/PanelSideBarLayout/PanelSideBarLayout.tsx
+++ src/lib/Layout/PanelSideBarLayout/PanelSideBarLayout.tsx
@@ -80,6 +80,7 @@ export const PanelSideBarLayout = <TPanelItemId extends string, TPanelItem>(prop
           { "section-tiles": renderFirstItemsLevelAsTiles },
         )}
       >
+        hello from timothy
         <PanelSideBar<TPanelItemId, TPanelItem> />
         {collapsible && !useToggleButton && <PanelSideBarToggle onClick={toggleSidebar} toggled={!isSidebarOpen} />}
         <PanelSideBarLayoutContent footer={footer} mainContentBodyRef={mainContentBodyRef}>
