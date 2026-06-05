c6b5ea341596fa979cd40f1cdd1ec6b2f4b6bdd8
Gianmarco Manni <gianmarco.manni@collana.com>
2026-06-05 18:54:41 +0200

    export component

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ export * from "./lib/Paging/Paging";
 
 // Panel sidebar layout
 export { PanelSidebarNavbar, PanelSidebarNavbarProps } from "./lib/Layout/PanelSideBarLayout/PanelSideBarNavbar";
+export * from "./lib/Layout/PanelSideBarLayout/NavbarLayout";
 export * from "./lib/Layout/PanelSideBarLayout/PanelSideBarLayout";
 export * from "./lib/Layout/PanelSideBarLayout/PanelSideBar/Context/PanelSideBarContext";
 export * from "./lib/Layout/PanelSideBarLayout/PanelSideBar/Definitions/PanelItem";
